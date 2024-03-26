a8eaef6a93a240107cc0dbc05e25ff45de1780ee
SanjaMilo <sanjamiloradovic@yahoo.com>
2024-03-26 13:36:15 +0100

    add helmet to set content security policy and remove fonts error

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import { recipesRouter } from './routes/recipesRouter.js';
 import { usersRouter } from './routes/usersRouter.js';
+import helmet from 'helmet';
 
 // instance of express
 const app = express();
@@ -14,11 +15,20 @@ const PORT = process.env.PORT || 5000;
 const environment = process.env.NODE_ENV;
 
 // Middleware
+// use helmet to set Content Security Policy
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'none'"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+    },
+}));
+// use json
 app.use(express.json());
+// use cors
 app.use(cors({
     origin: "https://myvegancookbook.onrender.com"
 }));
-// logging middleware
+// use morgan logging middleware
 if (environment === 'development') {
     app.use(morgan('dev'));
 }
